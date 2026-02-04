bcfe8b29825f45485adb1e8fc3127e8f33719ec7
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2026-02-04 14:46:36 +0800

    Add files via upload

---
+++ joseph web portfolio 2026/js/script.js
@@ -0,0 +1,14 @@
+function myName(){
+    document.getElementById("myName").innerHTML="Joseph Anthony V. Morales";
+}
+myName();
+
+function myPosition(){
+    document.getElementById("myPosition").innerHTML="Web Development | Graphic Design";
+}
+myPosition();
+
+function myBio(){
+    document.getElementById("myBio").innerHTML="Hi, I'm Joseph, a passionate web developer and graphic designer based in the Philippines.";
+}
+myBio();
