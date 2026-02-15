28d21993460ec8adcd4e3e371fb65eb3279dfeb4
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2026-02-15 08:45:51 +0800

    Update index.js

--- joseph web portfolio 2026/js/index.js
+++ joseph web portfolio 2026/js/index.js
@@ -1,5 +1,10 @@
 import { exp_nav_func } from "../js/nav.js";
 import { exp_main_func } from "../js/main.js";
+import { exp_section_func } from "./section.js";
 
 document.body.insertAdjacentHTML("beforeend", exp_nav_func());
+console.log(exp_nav_func());
 document.body.insertAdjacentHTML("beforeend", exp_main_func());
+console.log(exp_main_func());
+document.body.insertAdjacentHTML("beforeend",exp_section_func());
+console.log(exp_section_func());
