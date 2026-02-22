23218407bb8a2ff7c0fde4dab0509d9b37d28dec
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2026-02-22 14:29:17 +0800

    Update index.js

--- joseph web portfolio 2026/js/index.js
+++ joseph web portfolio 2026/js/index.js
@@ -2,9 +2,10 @@ import { exp_nav_func } from "../js/nav.js";
 import { exp_main_func } from "../js/main.js";
 import { exp_section_func } from "./section.js";
 
-document.body.insertAdjacentHTML("beforeend", exp_nav_func());
-console.log(exp_nav_func());
-document.body.insertAdjacentHTML("beforeend", exp_main_func());
-console.log(exp_main_func());
-document.body.insertAdjacentHTML("beforeend",exp_section_func());
-console.log(exp_section_func());
+// If nav should go somewhere specific, insert it properly.
+// But DO NOT inject directly into body unless intentional.
+
+document.getElementById("app").innerHTML =
+    exp_nav_func() +
+    exp_main_func() +
+    exp_section_func();
