a20e360caf840147442330ed6be47494ed630647
JosephMorales28 <josephmo2893@gmail.com>
2026-03-30 19:23:28 +0800

    re-add vite config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {defineConfig} from 'vite';
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        blog: 'blog.html',
+        contact: 'contact.html',
+        project: 'project.html',
+        service: 'service.html'
+      }
+    }
+  },
+  base:'./'
+});
