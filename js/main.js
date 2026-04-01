baefd0508276f290fe38a84cf6d7cdfb1eecfbc6
JosephMorales28 <josephmo2893@gmail.com>
2026-04-01 10:50:51 +0800

    update

--- js/main.js
+++ js/main.js
@@ -90,14 +90,28 @@ export function exp_main_func(){
             {label:"Version Patch", href:"/version_patch.html"},
             {label:"Community", href:"/community.html"}
         ];
-        const blogversion=["v1.1.4","v1.1.3","v1.1.2","v1.1.1","v1.1.0"];
+        const versionpatch=["v1.1.5","v1,1,4","v1.1.3","v1.1.2","v1.1.1","v1.1.0"];
+        const blogversion=[
+                          {patch:"v1.1.5",name:"Update patch changes"},
+                          {patch:"v1.1.4",name:"Add blog site"},
+                          {patch:"v1.1.3",name:"Add Section tag"},
+                          {patch:"v1.1.2",name:"Font Changes"},
+                          {patch:"v1.1.1",name:"Form Tag on Index"},
+                          {patch:"v1.1.0",name:"Upgrade Website portfolio"}
+        ];
 
         const blogMenuItems=blogLinks.map((item)=>{
             return `<li><a href="${item.href}">${item.label}</a></li>`;
         }).join("");
 
         const blogversionlist=blogversion.map((item)=>{
-            return `<li>${item}</li>`;
+            return `<li><div id="versionlist">
+                            <code style="align-self:flex-start">${item.patch}</code>
+                            <div id="title_author">
+                               <h3>- ${item.name}</h3>
+                               <p> Joseph Morales</p>
+                            </div></div>
+                    </li>`;
         }).join("");
         
         return `
@@ -111,10 +125,31 @@ export function exp_main_func(){
                     <div id="blogleft">
                        <figure>
                           <picture>
+                             <source srcset="./img/banner/websiteportfoliov1.1.5.webp" type="image/webp">
+                             <img src="./img/banner/websiteportfoliov1.1.5.webp" alt="banner" loading="lazy" fetchpriority="high">
+                          </picture>
+                          <figcaption>Release version of ${versionpatch[0]}</figcaption>
+                       </figure>
+                       <h2>Patch Changes:</h2>
+                       <p>- Update blog style and version list.</p>
+                       <p>- Fix error bug script.</p>
+                       <p>- Add new .htaccess file</p>
+                       <p>- Update README.md</p>
+                       <p>- Relocate Folders for image and fonts</p>
+                       <h2>Screenshot:</h2>
+                       <figure>
+                          <picture>
+                             <source srcset="./img/banner/screenshot_v1.1.5.webp" type="image/webp">
+                             <img src="./img/banner/screenshot_v1.1.5.webp" alt="banner" loading="lazy" fetchpriority="high">
+                          </picture>
+                          <figcaption>Screenshot version of ${versionpatch[0]}</figcaption>
+                       </figure>
+                       <figure>
+                          <picture>
                              <source srcset="./img/banner/websiteportfoliov1.1.4.webp" type="image/webp">
                              <img src="./img/banner/websiteportfoliov1.1.4.webp" alt="banner" loading="lazy" fetchpriority="high">
                           </picture>
-                          <figcaption>Release version of ${blogversion[0]}</figcaption>
+                          <figcaption>Release version of ${versionpatch[1]}</figcaption>
                        </figure>
                        <h2>Features:</h2>
                        <p>- add blog site and update the script.</p>
@@ -125,7 +160,7 @@ export function exp_main_func(){
                              <source srcset="./img/banner/screenshot_v1.1.4.webp" type="image/webp">
                              <img src="./img/banner/screenshot_v1.1.4.webp" alt="banner" loading="lazy" fetchpriority="high">
                           </picture>
-                          <figcaption>Screenshot version of ${blogversion[0]}</figcaption>
+                          <figcaption>Screenshot version of ${versionpatch[1]}</figcaption>
                        </figure>
                     </div>
                     <div id="blogright">
